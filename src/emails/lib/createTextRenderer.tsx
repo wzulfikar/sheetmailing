f21fe5042caa4f1658893f015b5c41b0ef0005ec
Wildan Zulfikar <hey@wzulfikar.com>
2022-10-31 22:29:45 +0200

    Handle placeholders

--- src/emails/lib/createTextRenderer.tsx
+++ src/emails/lib/createTextRenderer.tsx
@@ -2,11 +2,19 @@ import { MjmlHtml } from "mjml-react/extensions";
 import sanitizeHtml from "sanitize-html";
 import { marked } from "marked";
 
+import defaultPlaceholders from "./placeholders";
+
 // eslint-disable-next-line react/display-name
 export const createTextRenderer = (placeholders) => (text: string) => {
-  const year = new Date().getFullYear();
-  const translatePlaceholders = text.replaceAll("{year}", year.toString());
-  const html = sanitizeHtml(marked.parse(translatePlaceholders));
-
+  const html = sanitizeHtml(marked.parse(withPlaceholders(text, placeholders)));
   return <MjmlHtml html={html} />;
 };
+
+function withPlaceholders(text, placeholders: { [key: string]: string } = {}) {
+  placeholders = { ...placeholders, ...defaultPlaceholders };
+  let translated = text;
+  for (const key in placeholders) {
+    translated = translated.replaceAll(`{${key}}`, placeholders[key]);
+  }
+  return translated;
+}
